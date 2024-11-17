407a0e17a48be30b35fb148c05b27606440a8738
JestiferHarold <sanjay.pandian@tutamail.com>
2024-11-18 00:21:05 +0530

    Adding a new game (Rock, Paper and Scissors), Not fully completed

---
+++ Rock Paper Scissors/main.py
@@ -0,0 +1,7 @@
+from utils import clear_terminal, paper
+
+print(clear_terminal())
+print(paper())
+
+score = {"Computer" : 0, "Player" : 0}
+
---
+++ Rock Paper Scissors/utils.py
@@ -0,0 +1,36 @@
+import platform
+import os
+
+def find_os():
+    return platform.system()
+
+def clear_terminal():
+    if find_os().startswith("Windows"):
+        return os.system("cls")
+    return os.system("clear")
+
+def paper():
+
+    return """
+  _____________________
+ /                    /
+/                    /
+|                   |
+|                   |
+|                   |
+|      paper        |
+|                   |
+|                   |
+|                   |
+|                   |
+|                   |
+|___________________|
+"""
+def rock():
+
+    return """
+    __________
+  /
+ /    
+|
+"""
